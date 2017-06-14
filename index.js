644f8b039740a74dc0ae4bb2d71a07f57c4dc7af
KnowHow866 <accidental001@gmail.com>
2017-06-14 21:06:05 +0800

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.PORT || 5437;
+app.set('port',(process.env.PORT || 3000));
 
 app.use(express.static('./public'));
  
