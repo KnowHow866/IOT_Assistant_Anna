2fdc7addccbe180e67fd46753162d23fff90b6ca
KnowHow866 <accidental001@gmail.com>
2017-06-14 21:16:35 +0800

    origin

---
+++ index_test.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.set('port',(process.env.PORT || 3000));
+
+app.use(express.static('./public'));
+ 
+
+// 路由
+app.get("/test",function(req,res){
+	res.send("Hello");
+});
+
+app.listen(app.get('port'),function(req,res){
+	console.log("Server init at port "+ app.get('port'));
+});
+
